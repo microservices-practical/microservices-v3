bcba509eafdbb825684591e958be93124b00ce9d
mechero <moises.macero@gmail.com>
2017-06-26 06:49:02 +0200

    first commit

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -0,0 +1,20 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+
+public interface MultiplicationService {
+
+    /**
+     * Generates a random {@link Multiplication} object.
+     *
+     * @return a multiplication of randomly generated numbers
+     */
+    Multiplication createRandomMultiplication();
+
+    /**
+     * @return true if the attempt matches the result of the
+     *         multiplication, false otherwise.
+     */
+    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+}
